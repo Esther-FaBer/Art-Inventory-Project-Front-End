3e899ba7a3378766bf48b4afdab1019fbb7e5ec3
EstherFaBer <estherfaundez@gmail.com>
2026-07-29 22:41:07 +0100

    debugging header

--- Art Inventory Project Front End/src/components/Layout.tsx
+++ Art Inventory Project Front End/src/components/Layout.tsx
@@ -1,25 +1,31 @@
+import { NavLink } from 'react-router-dom';
 import Header from './Header';
-import Sidebar from './Sidebar';
 import './Layout.css';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="layout">
 
-      {/* Top bar - spans the full width */}
+      {/* Top bar - search, notifications, user menu */}
       <Header />
 
-      {/* Below the header - sidebar on left, page content on right */}
-      <div className="layout-body">
+      {/* Horizontal nav bar - logo on left, links across */}
+      <nav className="nav-bar">
+        <span className="nav-logo">ArtDB</span>
+        <div className="nav-links">
+          <NavLink to="/artworks" className="nav-link">Artworks</NavLink>
+          <NavLink to="/artists" className="nav-link">Artists</NavLink>
+          <NavLink to="/contacts" className="nav-link">Contacts</NavLink>
+          <NavLink to="/sales" className="nav-link">Sales</NavLink>
+          <NavLink to="/marketing" className="nav-link">Marketing</NavLink>
+          <NavLink to="/accounts" className="nav-link">Accounts</NavLink>
+        </div>
+      </nav>
 
-        <Sidebar />
-
-        {/* children is whatever page is currently active */}
-        <main className="layout-main">
-          {children}
-        </main>
-
-      </div>
+      {/* Page content */}
+      <main className="layout-main">
+        {children}
+      </main>
 
     </div>
   );
